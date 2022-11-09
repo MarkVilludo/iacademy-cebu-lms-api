582a1d06755c8022dce46bba1f6febf1533d6cee
MarkVilludo <markanthony.villudo@gmail.com>
2022-11-09 22:57:26 +0800

    .

--- database/migrations/2022_11_05_160025_create_payment_items_table.php
+++ database/migrations/2022_11_05_160025_create_payment_items_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->decimal('price', 10, 2);
             $table->text('remarks');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
