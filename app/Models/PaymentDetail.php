784195f9dab61da6e68418298ccad2b9721e86a1
MarkVilludo <markanthony.villudo@gmail.com>
2022-11-09 22:54:16 +0800

    .

--- app/Models/PaymentDetail.php
+++ app/Models/PaymentDetail.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
-use App;
+use App, Mail;
 
 class PaymentDetail extends Model
 {
