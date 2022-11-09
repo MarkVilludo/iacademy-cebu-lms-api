fb22699d3d55ff442ad7eb1b95c916c9c1b26c93
MarkVilludo <markanthony.villudo@gmail.com>
2022-11-09 23:05:19 +0800

    .

--- app/Http/Resources/Admissions/UploadedRequirementResource.php
+++ app/Http/Resources/Admissions/UploadedRequirementResource.php
@@ -14,6 +14,7 @@ class UploadedRequirementResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return $this->file;
     }
 }
--- app/Models/StudentInformationRequirement.php
+++ app/Models/StudentInformationRequirement.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class StudentInformationRequirement extends Model
 {
     use HasFactory;
+
+    public function file()
+    {
+        return $this->belongsTo(AdmissionFile::class, 'admission_file_id', 'id');
+    }
 }
