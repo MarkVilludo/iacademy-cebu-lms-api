50e524394997c25f36657270e2ab174ebbc41e42
MarkVilludo <markanthony.villudo@gmail.com>
2022-11-11 01:40:43 +0800

    .

--- resources/views/emails/admissions/acceptance_letter.blade.php
+++ resources/views/emails/admissions/acceptance_letter.blade.php
@@ -12,7 +12,7 @@
             <img style="display:block;margin:0 auto; width:800px" width="800" src="https://i.ibb.co/tPGck0d/Header.png"
                 alt="">
             <div class="" style="padding:20px;font-family:verdana;font-size:14px;">
-                { Hi Ms/Mr. {!! $information->first_name .' '. $information->last_name !!}!
+                Hi Ms/Mr. {!! $information->first_name .' '. $information->last_name !!}!
                 Acceptance Letter here.....
 
                 <br><br>
