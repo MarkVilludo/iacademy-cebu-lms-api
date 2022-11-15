05b61cee835ec5510981b1dc493f3a3bad40279c
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-11-15 10:35:29 +0800

    Updated Email Headers

--- resources/views/emails/admissions/reserved_slot.blade.php
+++ resources/views/emails/admissions/reserved_slot.blade.php
@@ -7,7 +7,7 @@
     <body>
         <div class="" style="width:100%;margin:0 auto;display:block;">
             <div class="" style="width:800px;display:block;margin:0 auto;">
-                <img style="display:block;margin:0 auto; width:800px" width="800" src="https://i.ibb.co/tPGck0d/Header.png" alt="">
+                <img style="display:block;margin:0 auto; width:800px" width="800" src="https://i.ibb.co/R6pS725/cebu-header.png" alt="">
                 <div class="" style="padding:20px;font-family:verdana;font-size:14px;">
                     Hi Ms/Mr. {!! $information->first_name .' '. $information->last_name !!}!
                     <br><br>
--- resources/views/emails/admissions/submit_information.blade.php
+++ resources/views/emails/admissions/submit_information.blade.php
@@ -9,7 +9,7 @@
 <body>
     <div class="" style="width:100%;margin:0 auto;display:block;">
         <div class="" style="width:800px;display:block;margin:0 auto;">
-            <img style="display:block;margin:0 auto; width:800px" width="800" src="https://i.ibb.co/tPGck0d/Header.png"
+            <img style="display:block;margin:0 auto; width:800px" width="800" src="https://i.ibb.co/R6pS725/cebu-header.png"
                 alt="">
             <div class="" style="padding:20px;font-family:verdana;font-size:14px;">
                 <div class="content-block"
