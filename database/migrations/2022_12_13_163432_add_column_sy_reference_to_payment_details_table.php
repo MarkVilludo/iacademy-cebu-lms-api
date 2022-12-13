473c81f4ef5d556fefe43426c5f8760a4881e4d7
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2022-12-13 14:41:03 +0800

    Update 2022_12_13_163432_add_column_sy_reference_to_payment_details_table.php

--- database/migrations/2022_12_13_163432_add_column_sy_reference_to_payment_details_table.php
+++ database/migrations/2022_12_13_163432_add_column_sy_reference_to_payment_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnOrderStudentInformationRequirementsTable extends Migration
+class AddColumnSyReferenceToPaymentDetailsTable extends Migration
 {
     /**
      * Run the migrations.
