eed50a572c91d4a0e2767b5b6bd4cc2c9db22e63
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-21 15:10:35 +0800

    Update registration_notification.blade.php

--- resources/views/emails/registrar/registration_notification.blade.php
+++ resources/views/emails/registrar/registration_notification.blade.php
@@ -15,14 +15,14 @@
                 Greetings {!! $information->first_name .' '. $information->last_name !!},
                 <br><br>                
                 <br><br>
-                Click on the payment link below to pay tuition online: <br />
+                Click on the button below to pay tuition online: <br /> <br />
                 <a href="http://103.225.39.200/cebu-iac-lms/site/student_tuition_payment/{{$information->slug}}"
                     style="text-align:center">
                     <button type="button"
                         style="text-decoration:none;display: inline-block;font-weight: 400;color: #fff;text-align: center;vertical-align: middle;border: 1px solid transparent;padding: 8px 12px;font-size: 16px;line-height: 1.6;border-radius: 0.25rem;background-color: #1c54a5;border: 1px solid #1c54a5;">
                         Pay Tuition Online</button>
                 </a>                
-                
+                <br /><br /><br />
                 Thank you
                 <br /><br /><br />
             </div>
