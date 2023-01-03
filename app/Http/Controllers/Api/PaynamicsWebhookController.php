6129f892d6b86a480c53f6a9652f4821fd457863
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-01-03 09:16:01 +0800

    Update PaynamicsWebhookController.php

--- app/Http/Controllers/Api/PaynamicsWebhookController.php
+++ app/Http/Controllers/Api/PaynamicsWebhookController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Models\{StudentInfoStatusLog};
 use App\Mail\Admissions\{AdmissionsNotificationEmail};
 
+use Mail;
 
 class PaynamicsWebhookController extends Controller
 {
